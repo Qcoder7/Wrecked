351ce7963af5890699b2fa406a2af1699a6b42ad
Qcoder7 <qcoder.wrecked@outlook.com>
2025-05-30 13:51:34 +0530

    Update page.jsx

--- app/entercode/page.jsx
+++ app/entercode/page.jsx
@@ -1,9 +1,12 @@
+import React, { Suspense } from 'react';
 import CheckEnctokenClient from './CheckEnctokenClient';
 
 export default function EnterCodePage() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-blue-800 to-purple-800 text-white flex items-center justify-center p-4">
-      <CheckEnctokenClient />
+      <Suspense fallback={<h1>Loading...</h1>}>
+        <CheckEnctokenClient />
+      </Suspense>
     </div>
   );
 }
